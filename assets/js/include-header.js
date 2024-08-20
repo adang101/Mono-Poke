2079f8660394b6f03757e2ff9e9383ec0c340f6d
Alvin Dang <alvin.dang@yahoo.com>
2024-08-20 15:30:47 -0700

    hero img WIP

--- assets/js/include-header.js
+++ assets/js/include-header.js
@@ -3,5 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => {
       document.body.insertAdjacentHTML("afterbegin", data);
+
+      // Re-initialize plugins after header is loaded
+      if (typeof $.fn.slicknav !== "undefined") {
+        $("#navigation").slicknav({
+          prependTo: ".mobile_menu",
+          closedSymbol: "+",
+          openedSymbol: "-",
+        });
+      }
+
+      // Call any other initialization functions here
+      if (typeof initializeOtherPlugins === "function") {
+        initializeOtherPlugins();
+      }
     });
 });
+
+// Function to initialize other plugins
+function initializeOtherPlugins() {
+  // Add initialization code for other plugins here
+  // For example:
+  // if (typeof $.fn.owlCarousel !== 'undefined') {
+  //   $('.owl-carousel').owlCarousel();
+  // }
+}
